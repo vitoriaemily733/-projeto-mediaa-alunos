055c3ad218a98e92f069ded370dbc67c2d0cb410
vitoriaemily733 <vitoriasantos4611520@gmail.com>
2024-11-20 08:48:10 -0300

    alterando

--- notas.py
+++ notas.py
@@ -7,6 +7,7 @@ nota3 = float(input("Digite a terceira nota:"))
 media = (nota1 + nota2 + nota3) / 3
 
 
+
 if media > 5.0 and media < 6.0:
     print("Recuperação")
 elif media >= 6.0:
@@ -14,8 +15,7 @@ elif media >= 6.0:
 else:
     print("Reprovado")
 
-
-  #Verificando se o aluno foi aprovado ou reprovado
+#Verificando se o aluno foi aprovado ou reprovado
 if media >= 6:
     print("Você foi aprovado!")
 else:
@@ -24,3 +24,6 @@ else:
 # Exibindo o resultado
 print(f"Sua média final é: {media:.2f}")
 
+
+
+  
