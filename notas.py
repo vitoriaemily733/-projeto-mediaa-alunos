45973438a5c5ba8ee415e87aee18551400d7dc36
vitoriaemily733 <vitoriasantos4611520@gmail.com>
2024-11-20 09:05:35 -0300

    refatoracao

--- notas.py
+++ notas.py
@@ -3,8 +3,8 @@ nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota:"))
 
-# Calculando a média
-media = (nota1 + nota2 + nota3) / 3
+# Calculando a média ponderada
+media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
 
 
 
